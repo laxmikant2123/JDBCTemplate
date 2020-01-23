6bc0c005d8954439ca2e39084325e1dadf4bcd7a
sid <sid@DESKTOP-09K96HS>
2020-01-23 09:47:32 +0530

    initial comit

---
+++ BootJDBC/src/main/java/com/lsg/jdbc/BootJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.lsg.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJdbcApplication.class, args);
+	}
+
+}
---
+++ BootJDBC/src/test/java/com/lsg/jdbc/BootJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsg.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
